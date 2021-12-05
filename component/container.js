5a64eac8f5be76d4bdbe1d49164d1396f2252a2a
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-06 02:17:02 +0530

    preloader removed

--- component/container.js
+++ component/container.js
@@ -1,12 +1,12 @@
 
 import Gtm from './gtm'
 import Navbar from './navbar'
-// import Preload from './preload'
+
 export default function Container() {
     return (
         <>
             <Gtm/>
-            // <Preload/>
+            
             <Navbar/>
         </>
     )
