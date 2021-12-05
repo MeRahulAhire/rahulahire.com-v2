fd8b15c1af594477a79dee44198de77556920808
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-06 00:26:02 +0530

    ddbjs meta tag fix

--- pages/dynamodb.js
+++ pages/dynamodb.js
@@ -8,7 +8,7 @@ export default function Dynamodb() {
     <div>
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>The Ultimate DynamoDB Course</title>
         <meta name="theme-color" content="#212121" />
