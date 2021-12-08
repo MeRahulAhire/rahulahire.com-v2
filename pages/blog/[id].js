56c2d941ba1185b5c38e94a1d61152d102acad47
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-08 17:53:23 +0530

    gssp setup

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,21 @@
 import { useRouter } from "next/router"
 
-export default function Id() {
-    const router = useRouter()
-    const blogPage = router.query.id
+export default function Id({blogPage}) {
+    
     return (
         <div>
            <h1>Page ID : {blogPage}</h1>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    // const router = useRouter()
+    // const blogPage = router.query.id
+    const id = context.params.id
+    return {
+        props:{
+            blogPage: id
+        }
+    }
 }
