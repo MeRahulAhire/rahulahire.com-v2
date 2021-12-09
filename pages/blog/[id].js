39b2c0344a010e97fff9f3eda0f9bb6545f20201
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-09 19:27:43 +0530

    vercel branch initial commit - removed unnecessary files

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -15,7 +15,6 @@ export default function Id({ blogPage, blogUrl }) {
 export async function getServerSideProps(context) {
   const id = context.params.id;
   const baseUrl = `https://blog.rahulahire.com/${id}`;
-  // const lambdaUrl = `https://04o7ho1666.execute-api.ap-south-1.amazonaws.com/blog`
   const res = (await axios.get(baseUrl)).data;
   console.log(id)
   return {
