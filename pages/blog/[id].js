cbc6a760ca1c3ebcd9b8f2ce671806455769fa69
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-09 18:08:27 +0530

    modified for netlify deployment

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -11,8 +11,8 @@ export default function Id({ blogPage }) {
 export async function getServerSideProps(context) {
   const id = context.params.id;
   const baseUrl = `https://blog.rahulahire.com/${id}`;
-  const lambdaUrl = `https://04o7ho1666.execute-api.ap-south-1.amazonaws.com/blog`
-  const res = (await axios.post(lambdaUrl, {url :baseUrl})).data;
+  // const lambdaUrl = `https://04o7ho1666.execute-api.ap-south-1.amazonaws.com/blog`
+  const res = (await axios.get(baseUrl)).data;
   console.log(id)
   return {
     props: {
