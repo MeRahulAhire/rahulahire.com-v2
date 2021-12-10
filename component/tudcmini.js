de7383cf9e019b47404e1404fcc25d7764a66132
Rahul Ahire <ahirerahul21@gmail.com>
2021-12-10 23:01:54 +0530

    final deploy before ad

---
+++ component/tudcmini.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Tudcmini() {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "auto",
+          backgroundColor: "#D71A69",
+          color: "white",
+          padding: "10px",
+          fontFamily: "Righteous",
+          margin: 0,
+          padding: 0,
+          alignItems: "center",
+          paddingLeft: "15px",
+          paddingTop: '15px',
+          paddingBottom: '15px',
+          paddingRight: '15px'
+        }}
+      >
+        Learn DynamoDB effortlessly with the &nbsp;  
+        <a
+          style={{ color: "white", textDecoration: "underline" }}
+          href="https://rahulahire.thinkific.com"
+        >
+          Ultimate DynamoDB Course
+        </a>
+      </div>
+    </>
+  );
+}
