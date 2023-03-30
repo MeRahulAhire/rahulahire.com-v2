6ff91edd3b99c8a36a80337f455fa762591e0125
Rahul Ahire <info@rahulahire.com>
2023-03-30 18:09:46 +0530

    updated the module/packages

--- component/home.js
+++ component/home.js
@@ -41,10 +41,10 @@ export default function Home() {
     <>
     
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Lobster&family=Open+Sans:wght@300;400;500&family=Righteous&display=swap" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Lobster&family=Open+Sans:wght@300;400;500&family=Righteous&display=swap"/>
         <link
           rel="shortcut icon"
           type="image/x-icon"
